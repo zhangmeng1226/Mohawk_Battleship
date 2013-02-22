cce1547b3d8f7575b94f8ec60f29ab2be6476b60
Ryan Albert <ryan.albert@mohawkcollege.ca>
2013-02-22 13:26:10 -0500

    Added competition and result states to the console class.

--- Battleship/BattleshipConsole.cs
+++ Battleship/BattleshipConsole.cs
@@ -7,33 +7,51 @@ using System.Drawing;
 
 namespace Battleship
 {
+    /**
+     * <summary>Provides an interactive console for the user.</summary>
+     */
     public class BattleshipConsole
     {
-        private BattleshipConfig config;
-        private string[] fsStrings = { "first", "second" };
+        private BattleshipConfig config; //The main configuration object
+        private string[] fsStrings = { "first", "second" }; //Strings used to denote "first" and "second"
 
+        /**
+         * <summary>Class constructor. Initializes the configuration object.</summary>
+         */
         public BattleshipConsole()
         {
             config = new BattleshipConfig(Environment.CurrentDirectory + "\\..\\config.ini");
         }
 
+        /**
+         * <summary>The base class for console states</summary>
+         */
         abstract class ConsoleState
         {
-            protected BattleshipConsole main;
-            protected string extraMenu = "";
-            protected string headerEnds = "=====================";
+            protected BattleshipConsole main; //Reference to the 'parent'
+            protected string extraMenu = ""; //Extra key options available to the user.
+            protected const string headerEnds = "====================="; //Styling used for header ends
 
             public ConsoleState(BattleshipConsole main)
             {
                 this.main = main;
             }
 
-            public void WriteChars(char c, int n)
+            /**
+             * <summary>Writes a specific character for a number of times.</summary>
+             * <param name="c">The character to print to the console.</param>
+             * <param name="n">The number of times to print the character</param>
+             */
+            protected void WriteChars(char c, int n)
             {
                 for (int i = 0; i < n; i++)
                     Console.Write(c);
             }
 
+            /**
+             * <summary>Called in the loop, used for input interactivity.</summary>
+             * <param name="input">The string entered by the user into the console</param>
+             */
             public ConsoleState GetInput(string input)
             {
                 switch (input)
@@ -46,6 +64,11 @@ namespace Battleship
                 return Response(input);
             }
 
+            /**
+             * <summary>Prints the specified text, with the specified ends, centered, to the console.</summary>
+             * <param name="text">The text to print centered.</param>
+             * <param name="ends">The ends used for styling.</param>
+             */
             public void WriteCenteredText(string text, string ends)
             {
                 int maxChars = Console.WindowWidth - ends.Length * 2;
@@ -67,26 +90,40 @@ namespace Battleship
                 }
             }
 
+            /**
+             * <summary>Called in the loop, this provides a standard way for user interactivity and display.</summary>
+             */
             public void Display()
             {
                 Console.Clear();
                 WriteCenteredText("BATTLESHIP COMPETITION FRAMEWORK", headerEnds);
                 StateDisplay();
-                //WriteCenteredText("MENU", headerEnds);
                 WriteCenteredText("[E]xit   [C]onfigure   " + extraMenu, "===");
                 Console.Write("Input : ");
             }
 
+            /**
+             * <summary>Overridden to provide specific printing related to the subclass.</summary>
+             */
             protected abstract void StateDisplay();
+
+            /**
+             * <summary>Called when input is not handled by the base class.</summary>
+             * <param name="input">The string entered by the user</param>
+             * <returns>The implementing class, null, or a new ConsoleState object. null will end the program.</returns>
+             */
             protected abstract ConsoleState Response(string input);
         }
 
+        /**
+         * <summary>Allows the user to select a battleship bot</summary>
+         */
         class BotChoiceState : ConsoleState
         {
 
-            private int opponentChoiceCount = 0;
-            private IBattleshipOpponent[] opponents = new IBattleshipOpponent[2];
-            private InputError error = InputError.none;
+            private int opponentChoiceCount = 0; //The current battleship bot the user is selecting.
+            private IBattleshipOpponent[] opponents = new IBattleshipOpponent[2]; //The battleship bots being selected.
+            private InputError error = InputError.none; //The error, based on user input.
 
             public BotChoiceState(BattleshipConsole main)
                 : base(main)
@@ -165,21 +202,72 @@ namespace Battleship
 
         class CompetitionState : ConsoleState
         {
+            private IBattleshipOpponent[] bots;
+            private Dictionary<IBattleshipOpponent, int> scores;
+
             public CompetitionState(BattleshipConsole main, IBattleshipOpponent[] opp) : base(main)
             {
-
+                extraMenu = "[S]election";
+                bots = opp;
             }
 
             protected override void StateDisplay()
             {
-
+                WriteCenteredText("Bot competition mode", headerEnds);
+                Console.WriteLine("Running the competition...\n\n");
+                BattleshipCompetition bc = new BattleshipCompetition(
+                 bots[0],
+                 bots[1],
+                 new TimeSpan(0, 0, 1),
+                 51,
+                 true,
+                 new Size(10, 10),
+                 2, 3, 3, 4, 5);
+                scores = bc.RunCompetition();
+                Console.WriteLine("Done! Press any key to view the final results...");
             }
 
             protected override ConsoleState Response(string input)
             {
-                return this;
+                switch (input)
+                {
+                    case "S":
+                        return new BotChoiceState(main);
+                }
+                return new ResultState(main, scores);
+            }
+        }
+
+        class ResultState : ConsoleState
+        {
+
+            Dictionary<IBattleshipOpponent, int> results;
+
+            public ResultState(BattleshipConsole main, Dictionary<IBattleshipOpponent, int> results)
+                : base(main)
+            {
+                this.results = results;
+                extraMenu = "[R]ematch   [S]election";
             }
 
+            protected override void StateDisplay()
+            {
+                WriteCenteredText("Battleship competition results", headerEnds);
+                foreach (var key in results.Keys.OrderByDescending(k => results[k]))
+                    Console.WriteLine(key.Name + " has scored " + results[key]);
+            }
+
+            protected override ConsoleState Response(string input)
+            {
+                switch (input)
+                {
+                    case "R":
+                        return new CompetitionState(main, results.Keys.ToArray());
+                    case "S":
+                        return new BotChoiceState(main);
+                }
+                return this;
+            }
         }
 
         public void Start()
@@ -197,22 +285,6 @@ namespace Battleship
                 if (state == null)
                     break;
             }
-
-            /*BattleshipCompetition bc = new BattleshipCompetition(
-                opponents[0],
-                opponents[1],
-                new TimeSpan(0, 0, 1),
-                51,
-                true,
-                new Size(10, 10),
-                2, 3, 3, 4, 5);
-
-            Dictionary<IBattleshipOpponent, int> scores = bc.RunCompetition();
-
-            foreach (var key in scores.Keys.OrderByDescending(k => scores[k]))
-                Console.WriteLine(key.Name + " has scored " + scores[key]);
-
-            Console.WriteLine("\n\nEnd of competition! Press a key to exit.");*/
 
             config.SaveConfigFile();
         }
