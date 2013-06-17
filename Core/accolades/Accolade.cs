2297133968497e3ed2ad6dfb30c542ffb41338f9
Ryan <ryan.albert@mohawkcollege.ca>
2013-06-17 14:58:01 +0300

    Provided compiled documentation from doxygen (.chm). Named the two compressed documents to provide a comparison between the two generators. See the Match class (the only class with complete documentation atm) for a good comparison.

--- Core/accolades/Accolade.cs
+++ Core/accolades/Accolade.cs
@@ -1,33 +1,10 @@
 ﻿namespace MBC.Core.Accolades
 {
     /// <summary>
-    /// The Accolade class is the base class for a number of classes that are used to describe a Round from
-    /// various events that occur within the Round.
+    /// Used as a base class that provides information on processed analysis of a <see cref="Round"/>.
     /// </summary>
     public abstract class Accolade
     {
-        /// <summary>
-        /// Gets a string that describes why this Accolade was generated and added.
-        /// </summary>
-        public abstract string Message
-        {
-            get;
-        }
 
-        /// <summary>
-        /// Gets a string that identifies the name of this Accolade.
-        /// </summary>
-        public abstract string Name
-        {
-            get;
-        }
-
-        /// <summary>
-        /// Gets a string that describes this Accolade.
-        /// </summary>
-        public abstract string Description
-        {
-            get;
-        }
     }
 }
