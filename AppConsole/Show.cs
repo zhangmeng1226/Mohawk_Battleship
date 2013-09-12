6690ea735c83dd4ae1f7ff2b9350b711c2925a25
Ryan <piknikco@gmail.com>
2013-09-12 12:09:18 -0400

    Console help messages fix
    Fixed the display of the 'help' command by showing descriptions. This
    fix also affects any calls to Show.ProperLine.

--- AppConsole/Show.cs
+++ AppConsole/Show.cs
@@ -67,6 +67,8 @@ namespace MBC.App.BattleshipConsole
                     Console.SetCursorPosition(cursorLeft, Console.CursorTop);
                     currentWidth = txtSplit[txtIdx].Length + 1;
                 }
+                Console.Write(txtSplit[txtIdx]);
+                Console.Write(' ');
             }
         }
     }
