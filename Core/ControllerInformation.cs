2cf5bd726240cc8834c7c70fb11cdd384f8c0363
Ryan <piknikco@gmail.com>
2013-09-15 22:45:06 -0400

    Fixed .DLL bot loading bug
    Fixed the issue where only a single bot would be loaded from a single
    .DLL file. Any and all classes that extend the "Controller" class will
    now be loaded.

--- Core/ControllerInformation.cs
+++ Core/ControllerInformation.cs
@@ -176,7 +176,6 @@ namespace MBC.Core
                                 cont);
                             results.Add(info);
                         }
-                        break;
                     }
                 }
             }
