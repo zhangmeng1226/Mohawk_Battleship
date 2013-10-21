9bbdee3d315f7317d936492b8b645c8b7713107a
Piknik <piknikco@gmail.com>
2013-10-21 10:21:21 -0400

    New basic layout has been produced.

---
+++ AppWPF/Converters/BoolToVisibility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MBC.App.WPF.Converters
+{
+    internal class BoolToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (string)value == Visibility.Visible.ToString() ? true : false;
+        }
+    }
+}
