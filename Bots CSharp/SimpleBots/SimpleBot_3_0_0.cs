608659be44bf3c5804c963d693d3168aa062b532
DWMiller <david.miller8@mohawkcollege.ca>
2015-01-28 15:30:13 -0500

    SimpleBot3, rudimentary hunting mode

--- Bots CSharp/SimpleBots/SimpleBot_3_0_0.cs
+++ Bots CSharp/SimpleBots/SimpleBot_3_0_0.cs
@@ -10,12 +10,16 @@ using System.Text;
 
 namespace MBC.Controllers {
     /// <summary>
-    ///     A simple bot which shoots randomly
+    ///     A simple bot with random shooting plus basic hunting logic
+    ///     Details of this bots logic:
+    ///     1. This bot always places it's ships in the same locations
+    ///     2. This bot begins by shooting randomly
+    ///     3. When a hit occurs, this bot will cease shooting randomly and target all surrounding coordinates
     /// </summary>
-    [Name("SimpleBot 2 - Hunting")]
+    [Name("SimpleBot 3.0 - Hunting")]
     [Version(3, 0)]
     [Capabilities(GameMode.Classic)]
-    [Description("A simple bot which fires randomly")]
+    [Description("A simple bot with random shooting plus basic hunting logic")]
     [Author(FirstName = "David", LastName = "Miller")]
     [AcademicInfo("Mohawk College", "Software Development", 4)]
     public class SimpleBot_3_0_0 : Controller2 {
@@ -33,6 +37,8 @@ namespace MBC.Controllers {
             // only executing on your own shots.
             Player.Match.OnEvent += matchBeginEvent;
             Player.Match.OnEvent += roundBeginEvent;
+
+            Player.Match.OnEvent += shipHitEvent;
 
             // Hooking methods to Player.OnEvent events means 
             // the method will only handle events specific to 
@@ -74,13 +80,29 @@ namespace MBC.Controllers {
         }
 
         /// <summary>
-        ///     Handles the PlayerTurnBeginEvent, indicating a player's turn has begun 
+        ///     Handles the PlayerTurnBeginEvent, indicating a bots turn has begun 
         /// </summary>
         /// <param name="ev">An object of type PlayerTurnBeginEvent</param>
         [EventFilter(typeof(PlayerTurnBeginEvent))]
         private void playerTurnBeginEvent(Event ev) {
             PlayerTurnBeginEvent playerTurnBeginEvent = (PlayerTurnBeginEvent)ev;
             this.turnStart();
+        }
+
+        /// <summary>
+        /// Handles the ShipHitEvent, indicating a ship has been hit
+        /// </summary>
+        /// <param name="ev"></param>
+        [EventFilter(typeof(ShipHitEvent))]
+        private void shipHitEvent(Event ev) {
+            ShipHitEvent shipHitEvent = (ShipHitEvent)ev;
+            Player owner = shipHitEvent.Ship.Owner; //who owns the hit ship
+
+            // We only care about hits on enemy ships
+            if (owner != Player) {
+                // An opponent's ship has been hit
+                this.hit(shipHitEvent.HitCoords, shipHitEvent.Ship.IsSunk());
+            }
         }
 
         /*******************************************
@@ -91,11 +113,13 @@ namespace MBC.Controllers {
         // These should do very little (and everything), existing primarily to excution bot actions 
         // in a logical order at an appropriate time.
 
-
         private Random rand;
 
         private int matchX; // we'll store the horizontal size of the game board here
         private int matchY; // we'll store the vertical size of the game board here
+
+        private List<Coordinates> shots; // A list of all available target coordinates, not including those on the hunting list
+        private List<Coordinates> huntingShots; // A list of coordinates surrounding locations we have hit, to be priortized as targets
 
         /// <summary>
         /// A new match has begun
@@ -112,20 +136,58 @@ namespace MBC.Controllers {
         private void newRound() {
             this.placeShips();
             this.populateShotList();
+            this.initializeHuntingList();
         }
 
         /// <summary>
         /// It is our turn, fire!
         /// </summary>
         private void turnStart() {
-            this.shoot();
+            Coordinates nextShot = this.getNextShot();
+            this.shoot(nextShot);
+        }
+
+        /// <summary>
+        /// We have hit an enemy ship
+        /// </summary>
+        /// <param name="coords">Coordinates of the shot</param>
+        /// <param name="sunk">Did the hit sink the ship?</param>
+        private void hit(Coordinates coords, bool sunk) {
+            // Lets take the coordinates surrounding our hit and add them to our hunting list
+            List<Coordinates> surroundingShots = this.getSurroundingShots(coords);
+            moveShotToHuntingList(surroundingShots);
         }
 
         /*******************************************
          ********** Action Methods *****************
          *******************************************/
 
-        private List<Coordinates> shots;
+        /// <summary>
+        /// Takes coordinates and shoots at them
+        /// </summary>
+        /// <param name="shot"></param>
+        private void shoot(Coordinates shot) {
+            Player.Shoot(shot.X, shot.Y);
+
+            huntingShots.Remove(shot);
+            shots.Remove(shot);
+        }
+
+        /// <summary>
+        ///     Place ships in specific locations
+        /// </summary>
+        private void placeShips() {
+            // Smallest to largest            
+            ShipList.PlaceShip(Player.Ships, new Coordinates(9, 8), ShipOrientation.Vertical); // 2 length
+            ShipList.PlaceShip(Player.Ships, new Coordinates(0, 7), ShipOrientation.Horizontal); // 3 length
+            ShipList.PlaceShip(Player.Ships, new Coordinates(7, 1), ShipOrientation.Vertical); // 3 length
+            ShipList.PlaceShip(Player.Ships, new Coordinates(1, 9), ShipOrientation.Horizontal); // 4 length
+            ShipList.PlaceShip(Player.Ships, new Coordinates(2, 4), ShipOrientation.Horizontal); // 5 length
+        }
+
+        /*******************************************
+         ********** Helper Methods *****************
+         *******************************************/
 
         /// <summary>
         /// Populate our list of shot coordinates
@@ -141,29 +203,65 @@ namespace MBC.Controllers {
         }
 
         /// <summary>
-        /// Takes coordinates and shoots at them
+        /// Create a fresh hunting list
         /// </summary>
-        /// <param name="shot"></param>
-        private void shoot() {
-            int shotIndex = rand.Next(shots.Count);
-            Coordinates shot = shots[shotIndex];
-
-            Player.Shoot(shot.X, shot.Y);
-
-            shots.RemoveAt(shotIndex);           
+        private void initializeHuntingList() {
+            huntingShots = new List<Coordinates>();
         }
 
         /// <summary>
-        ///     Place ships in specific locations
+        /// Creates a list of Coordinates surrounding those provided
+        /// Does not validate to ensure they are within the bounds of the grid
         /// </summary>
-        private void placeShips() {
-            // Smallest to largest            
-            ShipList.PlaceShip(Player.Ships, new Coordinates(9, 8), ShipOrientation.Vertical); // 2 length
-            ShipList.PlaceShip(Player.Ships, new Coordinates(0, 7), ShipOrientation.Horizontal); // 3 length
-            ShipList.PlaceShip(Player.Ships, new Coordinates(7, 1), ShipOrientation.Vertical); // 3 length
-            ShipList.PlaceShip(Player.Ships, new Coordinates(1, 9), ShipOrientation.Horizontal); // 4 length
-            ShipList.PlaceShip(Player.Ships, new Coordinates(2, 4), ShipOrientation.Horizontal); // 5 length
+        /// <param name="shot"></param>
+        /// <returns></returns>
+        private List<Coordinates> getSurroundingShots(Coordinates shot) {
+            List<Coordinates> surroundingShots = new List<Coordinates>();
+
+            surroundingShots.Add(new Coordinates(shot.X + 1, shot.Y));
+            surroundingShots.Add(new Coordinates(shot.X - 1, shot.Y));
+            surroundingShots.Add(new Coordinates(shot.X, shot.Y + 1));
+            surroundingShots.Add(new Coordinates(shot.X, shot.Y - 1));
+
+            return surroundingShots;
         }
 
+        /// <summary>
+        /// Finds and removes a shot from the shot List and adds it to the hunting list
+        /// </summary>
+        private void moveShotToHuntingList(Coordinates shot) {
+            if (shots.Remove(shot)) {
+                huntingShots.Add(shot);
+            }
+        }
+
+        /// <summary>
+        /// Remove a list of shots from the shot list and add them to the hunting list
+        /// </summary>
+        /// <param name="shots"></param>
+        private void moveShotToHuntingList(List<Coordinates> shots) {
+            shots.ForEach(moveShotToHuntingList);
+        }
+
+        /// <summary>
+        /// Get the next shot, from the hunting list if available, randomly if not
+        /// </summary>
+        /// <returns></returns>
+        private Coordinates getNextShot() {
+            if (huntingShots.Count > 0) {
+                return this.getNextHuntingShot();
+            }
+
+            int shotIndex = rand.Next(shots.Count);
+            return shots[shotIndex];
+        }
+
+        /// <summary>
+        /// Take a shot from the hunting list
+        /// </summary>
+        /// <returns></returns>
+        private Coordinates getNextHuntingShot() {
+            return huntingShots.First();
+        }
     }
 }
