b410353f01efe39d5587e7379a4e0c994c39c187
Alex Radulescu <radulescualex@gmail.com>
2015-03-07 15:03:38 -0500

    Battleship using Windows Forms

---
+++ AppForm/Controls/Cell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MBC.Shared;
+
+namespace MBC.App.FormBattleship.Controls
+{
+
+    class Cell : Button
+    {
+        public Coordinates Coordinate{ get; set; }
+    }
+}
---
+++ AppForm/FormBattleship.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MBC.App.FormBattleship
+{
+    public partial class FormBattleship : Form
+    {
+        
+        public FormBattleship()
+        {
+            InitializeComponent();
+        }
+    }
+}
